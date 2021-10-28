26c0f269c564fbafdcdcdc52dfb5f0c49ad96436
Ricardo Ramirez <ricardo.ramirezr@elektra.com.mx>
2021-10-27 20:08:01 -0500

    implementing api calls

--- TheMovieDb/MovieAPI/MovieDBAPI.swift
+++ TheMovieDb/MovieAPI/MovieDBAPI.swift
@@ -6,3 +6,73 @@
 //
 
 import Foundation
+
+struct MovieDBAPI {
+    static let baseUrl = "https://api.themoviedb.org/3"
+    static let apiKey = "f6cd5c1a9e6c6b965fdcab0fa6ddd38a"
+    
+    struct GetTrendingMovies: RequestType {
+        typealias ResponseType = MovieListResponse
+        
+        var data = RequestData(
+            path: "\(MovieDBAPI.baseUrl)/trending/movie/day",
+            queryParams: [
+                "api_key": MovieDBAPI.apiKey,
+                "language": "en",
+                "region": "US"
+            ]
+        )
+    }
+    
+    struct GetNowPlayingMovies: RequestType {
+        typealias ResponseType = MovieListResponse
+        
+        var data = RequestData(
+            path: "\(MovieDBAPI.baseUrl)/movie/now_playing",
+            queryParams: [
+                "api_key": MovieDBAPI.apiKey,
+                "language": "en",
+                "region": "US"
+            ]
+        )
+    }
+    
+    struct GetPopularMovies: RequestType {
+        typealias ResponseType = MovieListResponse
+        
+        var data = RequestData(
+            path: "\(MovieDBAPI.baseUrl)/movie/popular",
+            queryParams: [
+                "api_key": MovieDBAPI.apiKey,
+                "language": "en",
+                "region": "US"
+            ]
+        )
+    }
+    
+    struct GetTopRatedMovies: RequestType {
+        typealias ResponseType = MovieListResponse
+        
+        var data = RequestData(
+            path: "\(MovieDBAPI.baseUrl)/movie/top_rated",
+            queryParams: [
+                "api_key": MovieDBAPI.apiKey,
+                "language": "en",
+                "region": "US"
+            ]
+        )
+    }
+    
+    struct GetUpcomingMovies: RequestType {
+        typealias ResponseType = MovieListResponse
+        
+        var data = RequestData(
+            path: "\(MovieDBAPI.baseUrl)/movie/upcoming",
+            queryParams: [
+                "api_key": MovieDBAPI.apiKey,
+                "language": "en",
+                "region": "US"
+            ]
+        )
+    }
+}
