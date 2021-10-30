512b4a7d6cf4ae465558a1d2656cc4ef33104021
javierCueto <javelie@outlook.com>
2021-10-30 08:12:22 -0600

    - init configuration home
    - UICollectionView in home controller

--- TheMovieDb/Home/Contoller/HomeViewController.swift
+++ TheMovieDb/Home/Contoller/HomeViewController.swift
@@ -7,19 +7,45 @@
 
 import UIKit
 
-final class HomeViewController: UIViewController {
+final class HomeViewController: UICollectionViewController {
     
     // MARK: - Properties
+    private let cellId = "homeId"
     
     // MARK: - Life Cycle
+    init() {
+        let layout = UICollectionViewFlowLayout()
+        super.init(collectionViewLayout: layout)
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 8
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cellId, for: indexPath)
+        cell.backgroundColor = .red
+        return cell
+    }
+    
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         configureUI()
+        configureUICollection()
         // Do any additional setup after loading the view.
     }
     
     // MARK: - Helpers
+    private func configureUICollection() {
+        collectionView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: cellId)
+    }
     private func configureUI() {
+        navigationItem.title = "Movies"
         view.backgroundColor = .systemBackground
     }
     // MARK: - Actions
