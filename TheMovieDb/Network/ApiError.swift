ab98435960ab15968b44a7191a22563ea98cf1a0
mikedoprado <mike.doprado78@gmail.com>
2021-11-01 08:10:51 -0500

    michael-do-prado-add-network-layer

---
+++ TheMovieDb/Network/ApiError.swift
@@ -0,0 +1,28 @@
+//
+//  ApiError.swift
+//  TheMovieDb
+//
+//  Created by Michael do Prado on 11/1/21.
+//
+
+import Foundation
+
+enum ApiError: Error {
+    case requestFailed
+    case jsonConversionFailure
+    case invalidData
+    case responseUnsuccesful
+    case jsonParsingFailure
+}
+
+extension ApiError {
+    var localizedDescription: String {
+        switch self {
+        case .invalidData: return "Invalid Data"
+        case .jsonConversionFailure: return "JSON Conversion Failed"
+        case .jsonParsingFailure: return "JSON Parsing Failed"
+        case .requestFailed: return "Request Failed"
+        case .responseUnsuccesful: return "Response Unsuccessful"
+        }
+    }
+}
