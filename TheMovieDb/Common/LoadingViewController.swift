67f92c7cc448c00c5bbb97f745ab87fe2928dc93
Ricardo Ramirez <ricardo.ramirezr@elektra.com.mx>
2021-11-02 20:06:41 -0600

    adding loader

--- TheMovieDb/Common/LoadingViewController.swift
+++ TheMovieDb/Common/LoadingViewController.swift
@@ -7,3 +7,18 @@
 
 import UIKit
 
+class LoadingViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        let spinner = UIActivityIndicatorView()
+        spinner.translatesAutoresizingMaskIntoConstraints = false
+        spinner.startAnimating()
+        view.addSubview(spinner)
+        
+        NSLayoutConstraint.activate([
+            spinner.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            spinner.centerYAnchor.constraint(equalTo: view.centerYAnchor)
+        ])
+    }
+}
