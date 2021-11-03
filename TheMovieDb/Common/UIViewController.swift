66eae3c824210322fe15caad505fd175467802b0
Ricardo Ramirez <ricardo.ramirezr@elektra.com.mx>
2021-11-02 20:06:53 -0600

    new view controller functions

--- TheMovieDb/Common/UIViewController.swift
+++ TheMovieDb/Common/UIViewController.swift
@@ -6,3 +6,20 @@
 //
 
 import UIKit
+
+extension UIViewController {
+    func add(_ child: UIViewController) {
+        addChild(child)
+        view.addSubview(child.view)
+        child.didMove(toParent: self)
+    }
+
+    func remove() {
+        guard parent != nil else {
+            return
+        }
+        willMove(toParent: nil)
+        view.removeFromSuperview()
+        removeFromParent()
+    }
+}
