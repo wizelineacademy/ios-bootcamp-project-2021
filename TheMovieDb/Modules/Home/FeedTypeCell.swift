e38b0e6621cacb80aebb40a20b595d023d985ede
Ricardo Ramirez <ricardo.ramirezr@elektra.com.mx>
2021-11-02 20:07:38 -0600

    adding cell class

--- TheMovieDb/Modules/Home/FeedTypeCell.swift
+++ TheMovieDb/Modules/Home/FeedTypeCell.swift
@@ -6,3 +6,29 @@
 //
 
 import UIKit
+
+final class FeedTypeCell: UICollectionViewCell {
+    
+    static let cellIdentifier = "FeedTypeCell"
+    
+    @IBOutlet weak var titleContainer: UIView!
+    
+    @IBOutlet weak var typeTitle: UILabel!
+    
+    override var isSelected: Bool {
+        didSet {
+            if isSelected {
+                titleContainer.backgroundColor = .systemBlue
+            } else {
+                titleContainer.backgroundColor = .lightGray
+            }
+            
+        }
+    }
+    
+    func updateUI(withFeedTitle title: String) {
+        typeTitle.text = title
+        titleContainer.layer.cornerRadius = 10
+    }
+    
+}
