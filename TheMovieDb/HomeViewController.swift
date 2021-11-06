312663f86abd7c77826c6f8eec96b825c94326c3
Sandra Herrera <sherrera@insulet.com>
2021-11-05 20:11:03 -0700

    add-home-view: add url request type

--- TheMovieDb/HomeViewController.swift
+++ TheMovieDb/HomeViewController.swift
@@ -55,13 +55,3 @@ extension HomeViewController: UICollectionViewDataSource {
         fatalError("Unable to dequeue subclassed cell")
     }
 }
-
-extension UIView {
-    func anchor(top: NSLayoutYAxisAnchor, leading: NSLayoutXAxisAnchor, bottom: NSLayoutYAxisAnchor, trailing: NSLayoutXAxisAnchor, padding: UIEdgeInsets = .zero) {
-        translatesAutoresizingMaskIntoConstraints = false
-        topAnchor.constraint(equalTo: top, constant: padding.top).isActive = true
-        leadingAnchor.constraint(equalTo: leading, constant: padding.left).isActive = true
-        trailingAnchor.constraint(equalTo: trailing, constant: padding.right).isActive = true
-        bottomAnchor.constraint(equalTo: bottom, constant: padding.bottom).isActive = true
-    }
-}
---
+++ TheMovieDb/Networking/URLRequestType.swift
@@ -0,0 +1,16 @@
+//
+//  URLRequestType.swift
+//  TheMovieDb
+//
+//  Created by Sandra Herrera on 05/11/21.
+//
+
+import Foundation
+
+enum URLRequestType: String {
+    case topRated = "/3/movie/top_rated"
+    case upcoming = "/3/movie/upcoming"
+    case popular = "/3/movie/popular"
+    case nowPlaying = "/3/movie/now_playing"
+    case latest = "/3/movie/latest"
+}
