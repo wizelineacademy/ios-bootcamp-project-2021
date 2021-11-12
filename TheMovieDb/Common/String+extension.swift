b1ed4b01eb7dbc2c90f60aeb2107d946a762d05a
Ricardo Ramirez <ricardo.ramirezr@elektra.com.mx>
2021-11-11 18:59:50 -0600

    string extension to get strings

--- TheMovieDb/Common/String+extension.swift
+++ TheMovieDb/Common/String+extension.swift
@@ -6,3 +6,9 @@
 //
 
 import Foundation
+
+extension String {
+    var localized: String {
+        return NSLocalizedString(self, tableName: nil, bundle: Bundle.main, value: "", comment: "")
+    }
+}
