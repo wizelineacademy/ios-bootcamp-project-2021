a4aad76883e17b1ba4d1a7369ea9147c93fd1b47
Karla Rubiano <karla.rubiano@gmail.com>
2021-11-17 11:34:47 -0500

    Feat: Remove KingFisher interface and use NSCache.

--- TheMovieDb/Helpers/UIImageView+Extensions.swift
+++ TheMovieDb/Helpers/UIImageView+Extensions.swift
@@ -7,6 +7,8 @@
 
 import UIKit
 
+let imageCache = NSCache<AnyObject, AnyObject>()
+
 extension UIImageView {
     func setImage(path: String?) {
         guard let path = path, let url = URL(string: Constants.imageBaseUrl + path) else {
@@ -14,8 +16,26 @@ extension UIImageView {
             self.image = image
             return
         }
-        
-        self.kf.indicatorType = .activity
-        self.kf.setImage(with: url)
+        setImage(from: url)
+    }
+    
+    func setImage(from url: URL, mode: UIView.ContentMode = .scaleAspectFit) {
+        contentMode = mode
+        if let imageFromCache = imageCache.object(forKey: url as AnyObject) {
+            self.image = imageFromCache as? UIImage
+            return
+        }
+        URLSession.shared.dataTask(with: url) { data, response, error in
+            guard
+                let httpURLResponse = response as? HTTPURLResponse, httpURLResponse.statusCode == 200,
+                let mimeType = response?.mimeType, mimeType.hasPrefix("image"),
+                let data = data, error == nil,
+                let imageToCache = UIImage(data: data)
+            else { return }
+            DispatchQueue.main.async() {
+                self.image = imageToCache
+                imageCache.setObject(imageToCache, forKey: url as AnyObject)
+            }
+        }.resume()
     }
 }
