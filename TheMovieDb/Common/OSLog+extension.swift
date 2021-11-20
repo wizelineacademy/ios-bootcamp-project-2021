560a522e899ffcd2ce1168153d7fb00ce4fce974
Ricardo Ramirez <ricardo.ramirezr@elektra.com.mx>
2021-11-19 20:22:01 -0600

    new oslog extension

--- TheMovieDb/Common/OSLog+extension.swift
+++ TheMovieDb/Common/OSLog+extension.swift
@@ -6,3 +6,11 @@
 //
 
 import Foundation
+import os.log
+
+extension OSLog {
+    private static var subSystem = Bundle.main.bundleIdentifier!
+    
+    static let networkRequest = OSLog(subsystem: subSystem, category: "networking")
+    static let viewModel = OSLog(subsystem: subSystem, category: "viewModel")
+}
