815027b135fce3a62a547e08131f8c276ff704a7
Ricardo Ramirez <ricardo.ramirezr@elektra.com.mx>
2021-11-23 19:23:22 -0600

    new method to load images

--- TheMovieDb/Common/UIImageView+extension.swift
+++ TheMovieDb/Common/UIImageView+extension.swift
@@ -6,3 +6,18 @@
 //
 
 import UIKit
+
+extension UIImageView {
+    func load(url: URL, completion: ((UIImage) -> Void)?) {
+        DispatchQueue.global().async { [weak self] in
+            if let data = try? Data(contentsOf: url) {
+                if let image = UIImage(data: data) {
+                    DispatchQueue.main.async {
+                        self?.image = image
+                        completion?(image)
+                    }
+                }
+            }
+        }
+    }
+}
