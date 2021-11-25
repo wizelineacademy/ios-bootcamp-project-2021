86c6fb9e937038b61e902aee2426ce28d5abd04c
Ricardo Ramirez <ricardo.ramirezr@elektra.com.mx>
2021-11-24 19:29:42 -0600

    remove storyboard

--- TheMovieDb/SceneDelegate.swift
+++ TheMovieDb/SceneDelegate.swift
@@ -16,6 +16,13 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
         // guard let _ = (scene as? UIWindowScene) else { return }
+        
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        window = UIWindow(frame: windowScene.coordinateSpace.bounds)
+        window?.windowScene = windowScene
+        let vc = HomeView()
+        window?.rootViewController = UINavigationController(rootViewController: vc)
+        window?.makeKeyAndVisible()
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
