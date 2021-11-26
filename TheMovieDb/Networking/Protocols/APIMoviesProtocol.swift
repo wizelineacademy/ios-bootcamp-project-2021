3deec61a0a74edffe1020e066584a233225532c4
javierCueto <javelie@outlook.com>
2021-11-25 22:16:45 -0700

    - implementing network layer with combine

--- TheMovieDb/Networking/Protocols/APIMoviesProtocol.swift
+++ TheMovieDb/Networking/Protocols/APIMoviesProtocol.swift
@@ -5,8 +5,9 @@
 //  Created by Javier Cueto on 01/11/21.
 //
 
-import Foundation
+import Combine
 
 protocol APIMoviesProtocol: AnyObject {
     func fetchData<T: Decodable>(endPoint: APIEndPoints, with parameters: APIParameters, completion: @escaping((Result<T, Error>) -> Void))
+    func fetchDataCombine<T: Decodable>(endPoint: APIEndPoints, with parameters: APIParameters) -> AnyPublisher<T, APIRequestError>
 }
--- TheMovieDb/Networking/Service/APIService.swift
+++ TheMovieDb/Networking/Service/APIService.swift
@@ -5,6 +5,7 @@
 //  Created by Javier Cueto on 27/10/21.
 //
 import Foundation
+import Combine
 
 class APIService: APIMoviesProtocol {
     
@@ -32,4 +33,56 @@ class APIService: APIMoviesProtocol {
         
     }
     
+    func fetchDataCombine<T: Decodable>(endPoint: APIEndPoints, with parameters: APIParameters) -> AnyPublisher<T, APIRequestError> {
+        let  urlBuild = APIBuild(with: parameters, with: endPoint)
+        guard let url = urlBuild.buildURL() else {
+            return Fail(outputType: T.self, failure: APIRequestError.badRequest).eraseToAnyPublisher()
+        }
+        
+        let decoder = JSONDecoder()
+        decoder.keyDecodingStrategy = .convertFromSnakeCase
+        return  URLSession.shared
+            .dataTaskPublisher(for: url)
+            .tryMap({ data, response in
+                if let response = response as? HTTPURLResponse, !(200...299).contains(response.statusCode) {
+                    throw self.httpError(response.statusCode)
+                }
+                return data
+            })
+            .decode(type: T.self, decoder: decoder)
+            .mapError { error in
+                self.handleError(error)
+            }
+            .receive(on: RunLoop.main)
+            .eraseToAnyPublisher()
+    }
+    
+}
+
+private extension APIService {
+    func httpError(_ statusCode: Int) -> APIRequestError {
+        switch statusCode {
+        case 400: return .badRequest
+        case 401: return .unauthorized
+        case 403: return .forbidden
+        case 404: return .notFound
+        case 402, 405...499: return .error4xx(statusCode)
+        case 500: return .serverError
+        case 501...599: return .error5xx(statusCode)
+        default: return .unknownError
+        }
+    }
+    
+    func handleError(_ error: Error) -> APIRequestError {
+        switch error {
+        case is Swift.DecodingError:
+            return .decodingError
+        case let urlError as URLError:
+            return .urlSessionFailed(urlError)
+        case let error as APIRequestError:
+            return error
+        default:
+            return .unknownError
+        }
+    }
 }
