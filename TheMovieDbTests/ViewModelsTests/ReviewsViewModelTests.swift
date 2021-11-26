09c6fc82b3c9cfcee5778ee4d113ba7d5b6411c1
Karla Rubiano <karla.rubiano@gmail.com>
2021-11-26 18:45:32 -0500

    Feat: Changed MockServices and ReviewsViewModelTests according with Combine.

--- TheMovieDbTests/ViewModelsTests/ReviewsViewModelTests.swift
+++ TheMovieDbTests/ViewModelsTests/ReviewsViewModelTests.swift
@@ -6,15 +6,16 @@
 //
 
 import XCTest
+import Combine
 @testable import TheMovieDb
 
 class ReviewsViewModelTests: XCTestCase {
 
     var viewModel: ReviewsViewModel?
+    var subscriptions = Set<AnyCancellable>()
     
     override func setUp() {
-        viewModel = ReviewsViewModel(facade: MockService())
-        viewModel?.movieID = 1
+        viewModel = ReviewsViewModel(id: 0, facade: MockService())
     }
     
     override func tearDown() {
@@ -22,10 +23,32 @@ class ReviewsViewModelTests: XCTestCase {
     }
     
     func testReviews() {
+        // Given
         let expectedReviews = [ReviewsDetails(author: "Karla", content: "Excelent"),
                                ReviewsDetails(author: "Daniela", content: "Bad movie")]
+        var resultReviews = [ReviewsDetails]()
+        let expectation = XCTestExpectation(description: "Reviews received")
+        expectation.expectedFulfillmentCount = 2
+        
+        viewModel?.$reviews
+            .sink(receiveCompletion: { completion in
+                switch completion {
+                case .failure:
+                    XCTFail("Failed to retrieve the reviews")
+                case .finished:
+                    break
+                }
+            }, receiveValue: { reviews in
+                resultReviews = reviews
+                expectation.fulfill()
+            }).store(in: &subscriptions)
+        
+        // When
         viewModel?.reviewsMovie()
-        XCTAssertEqual(viewModel?.reviews.count, expectedReviews.count)
-        XCTAssertEqual(viewModel?.reviews[0].author, expectedReviews[0].author)
+        
+        // Then
+        wait(for: [expectation], timeout: 1)
+        XCTAssertTrue(resultReviews.count == expectedReviews.count, "Different quantity of reviews.")
+        XCTAssertTrue(resultReviews.first?.author == expectedReviews.first?.author, "Different author for first review")
     }
 }
