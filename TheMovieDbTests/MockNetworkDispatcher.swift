a57f8053571f10ffdc6cfcc9c1a10fa4ba63ed4b
Ricardo Ramirez <ricardo.ramirezr@elektra.com.mx>
2021-11-28 20:12:46 -0600

    adding tests for moviedbapi

--- TheMovieDbTests/MockNetworkDispatcher.swift
+++ TheMovieDbTests/MockNetworkDispatcher.swift
@@ -6,3 +6,37 @@
 //
 
 import Foundation
+@testable import TheMovieDb
+
+class MockNetworkDispatcher: NetworkDispatcher {
+    
+    var data: Data?
+    
+    var error: Error?
+    
+    func dispatch<RequestType>(request: RequestType, completion: @escaping (Result<Data, Error>) -> Void) where RequestType: Request {
+        if let data = data {
+            completion(.success(data))
+        } else {
+            if let error = error {
+                completion(.failure(error))
+            } else {
+                completion(.failure(NSError(domain: "", code: 0, userInfo: nil)))
+            }
+            
+        }
+    }
+    
+}
+
+struct SimpleRequest: Request {
+    var queryParams: [String: String]?
+    
+    var path = ""
+    
+    typealias ResponseType = SimpleResponse
+}
+
+struct SimpleResponse: Decodable {
+    let testInt: Int
+}
