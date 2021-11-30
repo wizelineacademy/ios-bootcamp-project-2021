245c16955efa6d86a5cdc953a775d320694eafad
Ricardo Ramirez <ricardo.ramirezr@elektra.com.mx>
2021-11-29 20:45:03 -0600

    adding protocol to increase testability

--- TheMovieDb/Common/URLSession+extension.swift
+++ TheMovieDb/Common/URLSession+extension.swift
@@ -6,3 +6,23 @@
 //
 
 import Foundation
+
+protocol URLSessionProtocol {
+  func dataTask(
+    with request: URLRequest,
+    completionHandler: @escaping (Data?, URLResponse?, Error?) -> Void
+  ) -> URLSessionDataTaskProtocol
+}
+
+extension URLSession: URLSessionProtocol {
+    func dataTask(with request: URLRequest, completionHandler: @escaping (Data?, URLResponse?, Error?) -> Void) -> URLSessionDataTaskProtocol {
+        let task: URLSessionDataTask = dataTask(with: request, completionHandler: completionHandler)
+        return task as URLSessionDataTaskProtocol
+    }
+}
+
+protocol URLSessionDataTaskProtocol {
+    func resume()
+}
+
+extension URLSessionDataTask: URLSessionDataTaskProtocol { }
