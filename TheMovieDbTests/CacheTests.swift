d1b75921ceb9ba2c6cf231ca51043bd6606b514c
Ricardo Ramirez <ricardo.ramirezr@elektra.com.mx>
2021-11-29 20:47:10 -0600

    adding cache tests

--- TheMovieDbTests/CacheTests.swift
+++ TheMovieDbTests/CacheTests.swift
@@ -5,4 +5,43 @@
 //  Created by Ricardo Ramirez on 29/11/21.
 //
 
-import Foundation
+import XCTest
+@testable import TheMovieDb
+
+class CacheTests: XCTestCase {
+    
+    var sut: Cache<String, String>!
+
+    override func setUp() {
+        super.setUp()
+        sut = Cache<String, String>()
+    }
+    
+    override func tearDown() {
+        self.sut = nil
+        super.tearDown()
+    }
+    
+    func testInsert() {
+        sut.insert("test", forKey: "test")
+        XCTAssertEqual(sut.value(forKey: "test"), "test")
+    }
+    
+    func testRemove() {
+        sut.insert("test", forKey: "test")
+        sut.removeValue(forKey: "test")
+        XCTAssertNil(sut.value(forKey: "test"))
+    }
+    
+    func testRemoveWithSubscript() {
+        sut["test"] = "test"
+        sut["test"] = nil
+        XCTAssertEqual(sut["test"], nil)
+    }
+    
+    func testInsertWithSubscript() {
+        sut["test"] = "test"
+        XCTAssertEqual(sut["test"], "test")
+    }
+    
+}
