9809ce923651cacf05b0c431f7c56f9296bc5a92
Ricardo Ramirez <ricardo.ramirezr@elektra.com.mx>
2021-11-30 16:01:43 -0600

    new image loader class

--- TheMovieDb/Common/ImageLoader.swift
+++ TheMovieDb/Common/ImageLoader.swift
@@ -6,3 +6,41 @@
 //
 
 import Foundation
+import UIKit
+
+protocol ImageProvider {
+    func getImage(withURL url: URL, completion: @escaping (UIImage?) -> Void)
+}
+
+class ImageLoader: ImageProvider {
+    
+    private let cache: Cache<String, UIImage>
+    
+    private let urlSession: URLSessionProtocol
+    
+    init(urlSession: URLSessionProtocol = URLSession.shared, cache: Cache<String, UIImage> = Cache<String, UIImage>()) {
+        self.urlSession = urlSession
+        self.cache = cache
+    }
+    
+    func getImage(withURL url: URL, completion: @escaping (UIImage?) -> Void) {
+        if let cached = cache[url.absoluteString] {
+            completion(cached)
+        } else {
+            urlSession.dataTask(with: URLRequest(url: url)) { [weak self] data, _, _ in
+                if let data = data,
+                   let image = UIImage(data: data) {
+                    self?.cache[url.absoluteString] = image
+                    DispatchQueue.main.async {
+                        completion(image)
+                    }
+                } else {
+                    DispatchQueue.main.async {
+                        completion(nil)
+                    }
+                }
+            }.resume()
+        }
+    }
+    
+}
