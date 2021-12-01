432831a86361b39f8791c0600847c712f7767d71
Jonathan <rockjon.skroc@gmail.com>
2021-12-01 12:39:37 -0600

    [UPDATE]: image case refactor

--- TheMovieDb/Commons/UImage+Cache.swift
+++ TheMovieDb/Commons/UImage+Cache.swift
@@ -6,10 +6,24 @@
 //
 
 import UIKit
-var imageCache = NSCache<AnyObject, AnyObject>()
+
+var imageCache: NSCache<AnyObject, AnyObject> = {
+    let cache = NSCache<AnyObject, AnyObject>()
+    cache.countLimit = config.countLimit
+    return cache
+}()
+
+let config: Config = Config(countLimit: 100, memoryLimit: 1024 * 1024 * 100)
+
+struct Config {
+    let countLimit: Int
+    let memoryLimit: Int
+
+    static let defaultConfig = Config(countLimit: 100, memoryLimit: 1024 * 1024 * 100) // 100 MB
+}
 
 extension UIImageView {
-
+    
     func loadImage(urlString: String) {
         
         if let cacheImage = imageCache.object(forKey: urlString as AnyObject) as? UIImage {
